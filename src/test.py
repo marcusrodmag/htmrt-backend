b952fd12cbbf0dacca6c1d7c448909824849018a
Marcus R. Magalhães <marcus.magalhaes@engdb.com.br>
2019-12-03 03:47:45 -0300

    Env Name

--- src/test.py
+++ src/test.py
@@ -1,11 +1,12 @@
 
 import unittest
 from setup import app
+from config import Config
 
 class TestBackendApp(unittest.TestCase):
 
   def test_hello(self):
-    assert app.config.env_name() == "Development"
+    assert Config.env_name() == "Development"
 
 if __name__ == '__main__':
   unittest.main()
